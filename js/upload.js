a50dd33561e4b6cd6c7917fa0a24fb0cb96990eb
Richy <richardvinueza@gmail.com>
2019-03-06 17:04:22 +0100

    3ºEntrega,2ºTrimestre

--- js/upload.js
+++ js/upload.js
@@ -2,25 +2,28 @@ window.onload = initialize;
 var file;
 var storageRef;
 var imagesUpRef;
+var deleteButton;
 
 function initialize() {
     file = document.getElementById("file");
     file.addEventListener("change", uploadImagesToFirebase, false);
-
     storageRef = firebase.storage().ref();
-
     imagesUpRef = firebase.database().ref().child("imagesUp");
-
     showImagesFromFirebase();
 }
 
+var data;
+var show;
+var key;
+
 function showImagesFromFirebase() {
     imagesUpRef.on("value", function (snapshot) {
-        var data = snapshot.val();
-        var show = "";
-        for (var key in data) {
-            show += '<img width="300" height="300"  class="img-thumbnail" src="' + data[key].url + '"/>';
-
+        data = snapshot.val();
+        show = "";
+        for (key in data) {
+            show += '<img width="300" height="300"  class="img-thumbnail" src="' + data[key].url + '"/>' +
+            '<div>' + data[key].cocktail + '</div>' + 
+            '<div>' + data[key].ingredients + '</div>';
         }
         document.getElementById("images-from-firebase").innerHTML = show;
     })
@@ -29,147 +32,32 @@ function showImagesFromFirebase() {
 
 function uploadImagesToFirebase() {
     var uploadImage = file.files[0];
-
     var uploadTask = storageRef.child("images/" + uploadImage.name).put(uploadImage);
+   
 
     uploadTask.on("state_changed",
         function (snapshot) {
 
         }, function (error) {
-            alert("error");
+
 
         }, function () {
             uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
-                alert("Enviado");
                 console.log('File available at', downloadURL);
-                alert("url obtenido");
-                imagesUpRef.push({ name: uploadImage.name, url:downloadURL });
+
+                imagesUpRef.push({
+                    name: uploadImage.name,
+                    url: downloadURL,
+                    cocktail: document.getElementById("cocktail").value,
+                    ingredients : document.getElementById("ingredients").value
+                });
 
 
             });
-            /*
-            var downloadURL = uploadTask.snapshot.getdownloadURL;
-            alert("Enviada " + downloadURL);
-            crearNodoEnBDFirebase(uploadImage.name, downloadURL)
-            */
 
         });
 
 
 }
 
-/*
-function crearNodoEnBDFirebase( nameImage, downloadURL){
-    imagesUpRef.push({nombre: nameImage, url: downloadURL});
-
-
-}
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-var myFile;
-
-function initialize() {
-    initializeFirebase();
-
-    document.getElementById("my-file").addEventListener("change", uploadFile);
-    document.getElementById("Upload").addEventListener("submit", uploadPhoto)
-
-
-}
-
-function uploadPhoto(event) {
-    event.preventDefault();
-    var refImages = firebase.storage().ref().child("images/") //Tiene que ser igual a como lo tienes en tu base datos.
-    var uploadTask = refImages.put(myFile);
-
-    uploadTask.on("state_change",
-        function (snapshot) {
-            var subido = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-
-        },
-
-        function (error) {
-            alert("error");
-        },
-        function () {
-            uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
-                console.log("File avaible at", downloadURL);
-                document.getElementById("download-image").src = downloadURL;
-            });
-        });
-
-
-    refImages.put(myFile).then(function () {
-        console.log("File uploaded successfully")
-
-    });
-
-}
-
-
-
-function uploadFile(event) {
-    var imageToLoad = "<img src='' id='blah' alt='image'/>";
-    readURL(event.target);
-
-
-}
-
-function readURL(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
-        reader.readAsDataURL(input.files[0]);
-        myfile = input.files[0];
-
-    }
-
-}
-*/
 
